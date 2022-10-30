53a1dcc2ad2d1230f5ab8d21fdec3002fa1ade69
Adrian Martinez <martinaa@sunypoly.edu>
2022-10-30 15:29:22 -0400

    Update

--- main.py
+++ main.py
@@ -10,31 +10,37 @@ CS498 Capstone Project
 Fall 2022
 
 """
-# from tkinter import *
-
-from loadWindow import tkinterScreen
-
-
-
+import tkinter
+import tkintermapview
 
 def loadWindow():
-    # root window
-    window = tkinterScreen()
-    # set window size
-    window.geometry("800x400")
-    # set window title
-    window.title("CS 498 Capstone")
-    # set window icon
-    window.iconbitmap("SPLogo.ico")
-  
-  
+    root = tkinter.Tk()
+    root.title("CS 498")
+    root.geometry("900x700")
+    root.iconbitmap("SPLOGO.ico")
 
-    window.mainloop()
+    label = tkinter.LabelFrame(root)
+    label.pack(pady=20)
+
+    loadMap(label)
+
+    root.mainloop()
+
+def loadMap(label):
+    map_widget = tkintermapview.TkinterMapView(label, width=800, height=600, corner_radius=0)
+    map_widget.set_position(40.7128, -74.0060)
+    map_widget.set_zoom(10)
+    map_widget.pack()
+
     
 
-def main():
 
+def main():
     loadWindow()
+
+
+
+
 
 
 if __name__ == "__main__":
